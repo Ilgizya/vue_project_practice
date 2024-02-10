3b5d9465593e1dd8a33b3f031c60201315bb363c
Ilgizya <147098650+Ilgizya@users.noreply.github.com>
2024-02-11 00:27:58 +0300

    pre-training
    pre-training

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: '',
+  components: {
+  },
+  props: {
+  },
+  setup () {
+  }
+}
+
+<style lang="scss" scoped>
+  
+</script>
